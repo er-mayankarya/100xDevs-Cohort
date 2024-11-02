60e35e6be6d1d42a9c45c266bb3d8f27395eb0cb
Mayank Arya <ermayankarya7@gmail.com>
2024-11-02 14:00:08 +0530

    Updated

--- Week-11-React/Part-4-CustomHook/src/App.jsx
+++ Week-11-React/Part-4-CustomHook/src/App.jsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from 'react'
 
+
+/*
+// Implementation of useFetch - Custom Hook
+
 import { useFetch } from './hooks/useFetch';
 
 function App() {
@@ -7,6 +11,24 @@ function App() {
   return <div>
     {data}
   </div>
+}*/
+
+// Implementation of usePrev - Custom Hook (Important)
+
+import { usePrev } from './hooks/usePrev'
+
+function App() {
+  const [state, setState] = useState(0);
+  const prev = usePrev(state);
+
+  return <>
+  <p>{state}</p>
+  <button onClick={() => {
+    setState(curr => curr + 1)
+  }} >Click Me</button>
+  <p> The Previous Value Was {prev} </p>
+
+  </>
 }
 
 export default App
