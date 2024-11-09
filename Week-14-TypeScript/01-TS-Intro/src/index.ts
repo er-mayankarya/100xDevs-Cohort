86e996a226434f65474e98de871c9dc150a9d891
Mayank Arya <ermayankarya7@gmail.com>
2024-11-09 21:38:03 +0530

    Week 14 Type Script Intro added

---
+++ Week-14-TypeScript/01-TS-Intro/src/index.ts
@@ -0,0 +1,11 @@
+function greet(firstName: string){
+    console.log("hello " + firstName);
+}
+
+greet("Mayank Arya")
+
+function sum(a : number , b: number){
+    return a + b;
+}
+
+console.log(sum(1,2));
