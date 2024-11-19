463d7bcdeecb40261df8630ba367f0298d173f26
Mayank Arya <ermayankarya7@gmail.com>
2024-11-19 19:13:52 +0530

    Updated

--- Week-4-NodeJs-ExpressJs/01-NodeJS-Intro/index.js
+++ Week-4-NodeJs-ExpressJs/01-NodeJS-Intro/index.js
@@ -1,3 +1,18 @@
+/*
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'a.txt');
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
+*/
+
 const fs = require('fs');
 const { Command } = require('commander');
 const program = new Command();
