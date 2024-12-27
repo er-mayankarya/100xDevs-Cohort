552360692551182c7c41e1760ce0585d7dcf7364
Mayank Arya <ermayankarya7@gmail.com>
2024-12-27 19:26:52 +0530

    usePrev Custom Hook adfded in week-11 Part 4 of React

--- Week-11-React/React-Part4-CustomHooks/src/App.jsx
+++ Week-11-React/React-Part4-CustomHooks/src/App.jsx
@@ -1,3 +1,26 @@
+
+//usePrev() - Custom Hooks
+import React, { useState } from 'react'
+import { usePrev } from './hooks/usePrev';
+
+const App = () => {
+  const [state , setState] = useState(0);
+  const prev = usePrev(state);
+
+  return (
+    <div>
+      <h1>{state}</h1>
+      <button onClick={() => setState(c => c +1 )}>Click Me</button>
+      <h3>The Previous Value is {prev}</h3>
+    </div>
+  )
+}
+
+export default App
+
+
+/*
+//useFetch() - Custom Hook
 import { useFetch } from "./hooks/useFetch"
 
 
@@ -10,4 +33,4 @@ function App() {
   </div>
 }
 
-export default App
+export default Ap*/
