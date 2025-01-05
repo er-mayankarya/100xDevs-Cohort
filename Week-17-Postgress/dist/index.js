d4801f768af34d6e4cc70bab2aad222794bb1ea0
Mayank Arya <ermayankarya7@gmail.com>
2025-01-05 21:17:37 +0530

    SQL DB (Postgres) Added in Week-17

--- Week-17-Postgress/dist/index.js
+++ Week-17-Postgress/dist/index.js
@@ -8,32 +8,15 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-app.use(express_1.default.json());
-const pgClient = new pg_1.Client("postgresql://neondb_owner:VsWe54ARyuIm@ep-ancient-sunset-a5mu91l6.us-east-2.aws.neon.tech/neondb?sslmode=require");
-pgClient.connect();
-app.post('/signup', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const username = req.body.username;
-        const password = req.body.password;
-        const email = req.body.email;
-        const insertQuery = `INSERT INTO users (username , email , password ) VALUES ($1 , $2 , $3)`;
-        const response = yield pgClient.query(insertQuery, [username, email, password]);
-        res.json({
-            message: "You have Signed Up"
-        });
-    }
-    catch (e) {
-        console.log(e);
-        res.json({
-            message: "Error while signing up !!"
-        });
-    }
-}));
-app.listen(3000);
+require("dotenv/config");
+const pgClient = new pg_1.Client(process.env.DB_URI);
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield pgClient.connect();
+        const res = yield pgClient.query("SELECT * FROM users");
+        console.log(res.rows);
+    });
+}
+main();
--- Week-17-Postgress/src/index.ts
+++ Week-17-Postgress/src/index.ts
@@ -1,33 +1,12 @@
-import { Client } from "pg";
-import express from 'express';
+import { Client } from 'pg';
+import "dotenv/config";
 
-const app = express()
+const pgClient = new Client(process.env.DB_URI!);
 
-app.use(express.json());
+async function main(){
+    await pgClient.connect();
+    const res = await pgClient.query("SELECT * FROM users");
+    console.log(res.rows);
+}
 
-const pgClient = new Client("postgresql://neondb_owner:VsWe54ARyuIm@ep-ancient-sunset-a5mu91l6.us-east-2.aws.neon.tech/neondb?sslmode=require");
-pgClient.connect();
-
-app.post('/signup' , async (req , res) => {
-
-    try{
-        const username = req.body.username;
-        const password = req.body.password;
-        const email = req.body.email;
-
-        const insertQuery = `INSERT INTO users (username , email , password ) VALUES ($1 , $2 , $3)`
-
-        const response = await pgClient.query(insertQuery , [username , email , password]);
-
-        res.json({
-            message : "You have Signed Up"
-        })
-    }catch(e){
-        console.log(e);
-        res.json({
-            message : "Error while signing up !!"
-        })
-    }
-})
-
-app.listen(3000);
+main();
