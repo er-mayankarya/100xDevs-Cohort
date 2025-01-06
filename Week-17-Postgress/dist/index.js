b00eea1a9c5fe873d6df2a5df025328601935aa6
Mayank Arya <ermayankarya7@gmail.com>
2025-01-06 20:45:12 +0530

    Week-17 SQL Updated

--- Week-17-Postgress/dist/index.js
+++ Week-17-Postgress/dist/index.js
@@ -8,15 +8,45 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
 const pg_1 = require("pg");
 require("dotenv/config");
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
 const pgClient = new pg_1.Client(process.env.DB_URI);
-function main() {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield pgClient.connect();
-        const res = yield pgClient.query("SELECT * FROM users");
-        console.log(res.rows);
-    });
-}
-main();
+pgClient.connect();
+app.post("/signup", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { username, email, password } = req.body;
+        const { city, country, street, pincode } = req.body;
+        const insertQuery = `INSERT INTO users (username , email , password) VALUES ($1 , $2 , $3) RETURNING id`;
+        const response = yield pgClient.query(insertQuery, [
+            username,
+            email,
+            password,
+        ]);
+        const userId = response.rows[0].id;
+        const insertAddressQuery = `INSERT INTO addresses (city , country , street , pincode , user_id) VALUES ($1 , $2 , $3 , $4 , $5)`;
+        yield pgClient.query(insertAddressQuery, [
+            city,
+            country,
+            street,
+            pincode,
+            userId,
+        ]);
+        res.json({
+            message: "User Signed Up",
+        });
+    }
+    catch (error) {
+        console.log(error);
+        res.json({
+            message: "Error while signing up",
+        });
+    }
+}));
+app.listen(3000);
--- Week-17-Postgress/src/index.ts
+++ Week-17-Postgress/src/index.ts
@@ -1,12 +1,47 @@
-import { Client } from 'pg';
+import express from "express";
+import { Client } from "pg";
 import "dotenv/config";
 
+const app = express();
+app.use(express.json());
+
 const pgClient = new Client(process.env.DB_URI!);
+pgClient.connect();
 
-async function main(){
-    await pgClient.connect();
-    const res = await pgClient.query("SELECT * FROM users");
-    console.log(res.rows);
-}
+app.post("/signup", async (req, res) => {
 
-main();
+  try {
+    const { username, email, password } = req.body;
+
+    const { city, country, street, pincode } = req.body;
+
+    const insertQuery = `INSERT INTO users (username , email , password) VALUES ($1 , $2 , $3) RETURNING id`;
+    const response = await pgClient.query(insertQuery, [
+      username,
+      email,
+      password,
+    ]);
+    const userId = response.rows[0].id;
+
+    const insertAddressQuery = `INSERT INTO addresses (city , country , street , pincode , user_id) VALUES ($1 , $2 , $3 , $4 , $5)`;
+    await pgClient.query(insertAddressQuery, [
+      city,
+      country,
+      street,
+      pincode,
+      userId,
+    ]);
+
+    res.json({
+      message: "User Signed Up",
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.json({
+      message: "Error while signing up",
+    });
+  }
+});
+
+app.listen(3000);
