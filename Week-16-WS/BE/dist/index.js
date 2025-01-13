61f3cfb22c7086e716f529a7f1266a304f075ec0
Mayank Arya <ermayankarya7@gmail.com>
2025-01-13 20:13:08 +0530

    Week 16 WS Updated

--- Week-16-WS/BE/dist/index.js
+++ Week-16-WS/BE/dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
-const wss = new ws_1.WebSocketServer({ port: 8080 });
+const wss = new ws_1.WebSocketServer({ port: 3090 });
 wss.on('connection', (socket) => {
     socket.on("message", (e) => {
         if (e.toString() === 'ping') {
--- Week-16-WS/FE/src/App.tsx
+++ Week-16-WS/FE/src/App.tsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
 
 
-function App() {
+function App(){
+  const [socket , setSocket] = useState();
+
+  function sendMessages(){
+    if (!socket) {
+      return
+    }
+    //@ts-ignore
+    socket.send("pong");
+
+  }
+
+  useEffect( () => {
+    const ws = new WebSocket("ws://localhost:3090");
+    //@ts-ignore
+    setSocket(ws);
+    
+    ws.onmessage = (ev) => {
+      alert(ev.data);
+    }
+  } , [])
 
   return <div>
-    Hello Mayank
+    <input type="text" placeholder="Enter" />
+    <button onClick={ sendMessages }>Send</button>
   </div>
 }
 
-export default App
+export default App;
