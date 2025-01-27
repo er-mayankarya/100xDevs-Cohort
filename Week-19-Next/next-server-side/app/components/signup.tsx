10fe03aec3b6e14a6fe66f83a65c03a9ed5b086e
Mayank Arya <ermayankarya7@gmail.com>
2025-01-27 19:54:17 +0530

    Updated

--- Week-19-Next/next-server-side/app/components/signup.tsx
+++ Week-19-Next/next-server-side/app/components/signup.tsx
@@ -7,21 +7,26 @@ export function SignUp() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  async function handler() {
+    await axios.post("http://localhost:3000/api/user", {
+      username,
+      password,
+    });
+  }
+
   return (
     <div className="flex flex-col justify-center h-screen">
       <div className="flex justify-center">
-        <div className="p-10 border rounded">
-          <br />
+        <div className="border p-8 rounded">
           <input
             onChange={(e) => {
               setUsername(e.target.value);
             }}
-            className="p-2 m-2 "
+            className="p-2 m-2"
             type="text"
             placeholder="username"
           />
           <br />
-
           <br />
           <input
             onChange={(e) => {
@@ -32,18 +37,9 @@ export function SignUp() {
             placeholder="password"
           />
           <br />
-          <div className="mt-4 flex justify-center ">
-            <button
-              onClick={() => {
-                axios.post("http://localhost:3000/api/user"),
-                  {
-                    username,
-                    password,
-                  };
-              }}
-            >
-              Sign Up
-            </button>
+
+          <div className=" mt-4 flex justify-center">
+            <button onClick={handler}>Sign Up</button>
           </div>
         </div>
       </div>
